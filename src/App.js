e1183969afaf00f55fca68e5804c272d25f382ae
Zokky2e <icro4.gaming@hotmail.com>
2022-04-15 11:09:06 +0200

    april15

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
-    <div>noice new app</div>
+    <Routes>
+      <Route path="/" element={<AllMeetupsPage/>}/>
+      <Route path="/new-meetup" element={<NewMeetupPage />}/>
+      <Route path="/favorites" element={<FavoritesPage />}/>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
-      <App />,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const app = createRoot(container);
+app.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>);
