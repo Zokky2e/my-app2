8f85f5fed4ebb28061ca670605bd83d2bfdea439
Zokky2e <icro4.gaming@hotmail.com>
2022-04-16 18:39:23 +0200

    april16

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import {FavoritesContextProvider} from "./store/favorites-context";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const app = createRoot(container);
 app.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>);
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>
+);
